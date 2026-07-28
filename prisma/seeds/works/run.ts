2d24d40e5d9f5af9b03969ae6678747d5937eee5
michfour38 <michellefourie38@gmail.com>
2026-07-28 19:54:31 +0200

    Run WORKS locale seed

--- prisma/seeds/works/run.ts
+++ prisma/seeds/works/run.ts
@@ -1,7 +1,9 @@
+import { seedWorksLocales } from "./locales";
 import { seedWorksMarkets } from "./markets";
 
 export async function seedWorks() {
   console.log("Running WORKS seeds...");
   await seedWorksMarkets();
+  await seedWorksLocales();
   console.log("Done: WORKS seeds");
 }
