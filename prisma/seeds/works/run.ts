776b0f1b09422021516fdd6dd46de83a86d55e48
michfour38 <michellefourie38@gmail.com>
2026-07-29 08:08:04 +0200

    Wire WORKS provider type seeds

--- prisma/seeds/works/run.ts
+++ prisma/seeds/works/run.ts
@@ -1,11 +1,13 @@
 import { seedWorksCategories } from "./categories";
 import { seedWorksLocales } from "./locales";
 import { seedWorksMarkets } from "./markets";
+import { seedWorksProviderTypes } from "./provider-types";
 
 export async function seedWorks() {
   console.log("Running WORKS seeds...");
   await seedWorksMarkets();
   await seedWorksLocales();
   await seedWorksCategories();
+  await seedWorksProviderTypes();
   console.log("Done: WORKS seeds");
 }
