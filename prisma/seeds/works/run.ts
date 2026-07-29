b7a751fbf99b5520db2b9c19c33bb5f963e58fb9
michfour38 <michellefourie38@gmail.com>
2026-07-29 08:31:38 +0200

    Wire WORKS packaging format seed

--- prisma/seeds/works/run.ts
+++ prisma/seeds/works/run.ts
@@ -3,6 +3,7 @@ import { seedWorksCategories } from "./categories";
 import { seedWorksCredentialAuthorities } from "./credential-authorities";
 import { seedWorksLocales } from "./locales";
 import { seedWorksMarkets } from "./markets";
+import { seedWorksPackagingFormats } from "./packaging-formats";
 import { seedWorksProviderTypes } from "./provider-types";
 import { seedWorksServices } from "./services";
 
@@ -15,5 +16,6 @@ export async function seedWorks() {
   await seedWorksCredentialAuthorities();
   await seedWorksServices();
   await seedWorksCapabilities();
+  await seedWorksPackagingFormats();
   console.log("Done: WORKS seeds");
 }
