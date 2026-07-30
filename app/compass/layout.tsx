7cd4383631e274d5cd2af55d3c67721c93882e68
michfour38 <michellefourie38@gmail.com>
2026-07-30 10:05:49 +0200

    Load Compass Map ordering enhancer

--- app/compass/layout.tsx
+++ app/compass/layout.tsx
@@ -1,7 +1,14 @@
 import type { ReactNode } from "react";
 
+import { CompassMapOrderEnhancer } from "@/components/compass/CompassMapOrderEnhancer";
+
 import "./compass-legibility.css";
 
 export default function CompassLayout({ children }: { children: ReactNode }) {
-  return <div className="compass-legibility">{children}</div>;
+  return (
+    <div className="compass-legibility">
+      <CompassMapOrderEnhancer />
+      {children}
+    </div>
+  );
 }
