8ae4400b9a1cba08a505c1b57295f40ba8013650
michfour38 <michellefourie38@gmail.com>
2026-09-08 11:28:39 +0200

    Add Monetise stokvel signup page

---
+++ app/monetise-stokvel/page.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+
+import { SiteShell } from "@/components/site/site-shell";
+import { MonetiseStokvelSignup } from "@/components/site/monetise-stokvel-signup";
+
+export const metadata: Metadata = {
+  title: "Monetise Stokvel | Oremea",
+  description:
+    "A South African education stokvel forming 17 committed participants around a R2,000 monthly contribution.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default function MonetiseStokvelPage() {
+  const whatsappGroupUrl = process.env.MONETISE_STOKVEL_WHATSAPP_URL ?? "";
+
+  return (
+    <SiteShell>
+      <MonetiseStokvelSignup whatsappGroupUrl={whatsappGroupUrl} />
+    </SiteShell>
+  );
+}
