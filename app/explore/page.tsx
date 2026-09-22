e0834e0101bf8bd61b35cf4d49667e60bacf3754
michfour38 <michellefourie38@gmail.com>
2026-09-22 11:34:39 +0200

    Remove repeated negative boundaries from Explore

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -5,7 +5,6 @@ import { ExploreHero } from "@/components/site/sections/explore-hero";
 import { ExplorePrivacySafety } from "@/components/site/sections/explore-privacy-safety";
 import { ExploreStartingPoint } from "@/components/site/sections/explore-starting-point";
 import { ExploreWhatIs } from "@/components/site/sections/explore-what-is";
-import { ExploreWhatNot } from "@/components/site/sections/explore-what-not";
 
 export default function ExplorePage() {
   return (
@@ -18,7 +17,6 @@ export default function ExplorePage() {
 
       <ExploreWhatIs />
 
-      <ExploreWhatNot />
 
       <ExplorePrivacySafety />
     </SiteShell>
